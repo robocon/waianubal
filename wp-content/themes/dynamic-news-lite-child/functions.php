3644f2d9f5709eae84fa58f181ae1e6fe7fd44ea
Kritsanasak Kuntaros <roboconk@gmail.com>
2016-02-16 19:36:07 +0700

    - Add form for save Google Static Map API Key

--- wp-content/themes/dynamic-news-lite-child/functions.php
+++ wp-content/themes/dynamic-news-lite-child/functions.php
@@ -184,9 +184,12 @@ if( !function_exists('wbmap_func') ){
 
         if( !is_null($post) ){
 
+            $key = get_option('wb_staticmap_key');
+            if( empty($key) ){
+                return 'Please settings your Google Map API Key';
+            }
+
             $default_place = $atts['lat'].",".$atts['lng'];
-            $api_key = 'AIzaSyBKAT782gWUS7oykJNIf1_UdkaBjZ5oIKA'; // Real hosting
-            // $api_key = 'AIzaSyBfLZQBnJqdgk7b1NsHQbKOA8Qc6q5zoUE'; // Localhost
             $src = "https://maps.googleapis.com/maps/api/staticmap?center=".$atts['lat'].",".$atts['lng']."&zoom=15&size=600x300&maptype=roadmap&markers=color:red%7Clabel:%7C".$atts['lat'].",".$atts['lng']."&key=".$api_key;
             $img = '<a href="https://www.google.com/maps/place/'.$default_place.'" target="_blank"><img src="'.$src.'"></a>';
 
@@ -194,4 +197,52 @@ if( !function_exists('wbmap_func') ){
         }
 	}
 	add_shortcode( 'wbmap', 'wbmap_func' );
+}
+
+/* Form and Save data about GGMap API KEY */
+add_action('admin_menu', 'wb_staticmap_key');
+
+function wb_staticmap_key() {
+    add_submenu_page(
+        'options-general.php',
+        'GMap Static',
+        'GMap Static',
+        'manage_options',
+        'wb-static-map-key',
+        'wb_staticmap_key_callback'
+    );
+
+    add_action( 'admin_init', 'register_wb_staticmap_settings' );
+}
+
+function register_wb_staticmap_settings(){
+    register_setting( 'wb-staticmap-settings-group', 'wb_staticmap_key' );
+}
+
+function wb_staticmap_key_callback() {
+    ?>
+    <div class="wrap"><div id="icon-tools" class="icon32"></div>
+        <h2>Google Static API Key</h2>
+        <form action="options.php" method="post">
+            <?php settings_fields( 'wb-staticmap-settings-group' ); ?>
+            <?php do_settings_sections( 'wb-staticmap-settings-group' ); ?>
+            <table class="form-table">
+                <tbody>
+                    <tr>
+                        <th scope="row">
+                            <label for="wb_staticmap_key">Google Static Maps API</label>
+                        </th>
+                        <td>
+                            <input type="text" id="wb_staticmap_key" name="wb_staticmap_key" value="<?php echo esc_attr( get_option('wb_staticmap_key') ); ?>">
+                            <div class="">
+                                <a href="https://developers.google.com/youtube/registering_an_application" target="_blank">* Obtaining authorization credentials</a>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+    <?php
 }
